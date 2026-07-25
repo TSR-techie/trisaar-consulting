93845b0ab3139bdfb78712c080dbb78e6f6594ea
TriSaar <trivendar.rathore@outlook.com>
2026-07-25 18:07:53 +0530

    v.00.010_AddedDynamicHeaderFooter

---
+++ docs/assets/js/components.js
@@ -0,0 +1,23 @@
+async function loadComponent(id, file) {
+    try {
+        const response = await fetch(file);
+
+        if (!response.ok) {
+            throw new Error(`Failed to load ${file}`);
+        }
+
+        const html = await response.text();
+
+        document.getElementById(id).innerHTML = html;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadComponent("navbar", "./partials/navbar.html");
+    loadComponent("footer", "./partials/footer.html");
+
+});
